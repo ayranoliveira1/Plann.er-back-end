10d2c9c36ae580776359cee49060729d62ccb165
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-07-22 11:34:14 -0300

    Fix: fixing port error

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,6 @@ import { app } from "./app";
 import { env } from "./env";
 
 // iniciar o servidor
-app.listen({ port: env.PORT, host: "0.0.0.0" }).then(() => {
+app.listen({ port: env.PORT || 3333, host: "0.0.0.0" }).then(() => {
    console.log("Server running on port 3333");
 });
